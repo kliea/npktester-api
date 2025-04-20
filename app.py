3812d5b4478f1fa32a29831ef8517063cf527775
kliea <klinthjeraldqt@gmail.com>
2025-04-21 02:26:29 +0800

    updated the backend to improve feltilizer recommendation

--- app.py
+++ app.py
@@ -61,155 +61,48 @@ def predict():
     if not features or not isinstance(features, list) or len(features) != 3:
         return jsonify({'error': 'Invalid input. Must be a list of 3 NPK values.'}), 400
 
-    prediction = model.predict([features])[0]  # prediction[0] directly
+    prediction = model.predict([features])[0]
     response = {'prediction': prediction}
 
-    if prediction == 'maize':
-        nit = 197 - int(features[0])
-        phos= 70 - int(features[1])
-        pot = 180 - int(features[2])
-        
-        if nit > 10:
-            nit = 7.8
-        elif nit > 20:
-            nit = 15.6
-        elif nit > 30:
-            nit = 23.4
-        elif nit > 40:
-            nit = 31.2
-        elif nit > 50:
-            nit = 39
-        elif nit > 60:
-            nit = 46.8
-        elif nit > 70:
-            nit = 54.6
-        elif nit > 80:
-            nit = 62.4
-        elif nit > 90:
-            nit = 70.2
-        elif nit > 100:
-            nit = 78
-            
-        if phos > 10:
-            phos = 18
-        elif phos > 20:
-            phos = 36
-        elif phos > 30:
-            phos = 54
-        elif phos > 40:
-            phos = 72
-        elif phos > 50: 
-            phos = 90
-        elif phos > 60:
-            phos = 108
-        elif phos > 70:
-            phos = 126
-        elif phos > 80:
-            phos = 144
-        elif phos > 90:
-            phos = 162
-        elif phos > 100:
-            phos = 180
-            
-        if pot > 10:
-            pot = 6
-        elif pot > 20:
-            pot = 12
-        elif pot > 30:
-            pot = 18
-        elif pot > 40:
-            pot = 24
-        elif pot > 50: 
-            pot = 30
-        elif pot > 60:
-            pot = 36
-        elif pot > 70:
-            pot = 42
-        elif pot > 80:
-            pot = 48
-        elif pot > 90:
-            pot = 54
-        elif pot > 100:
-            pot = 60
-        
-        
-        response['needed_nutrients'] = {
-            'Urea': nit,
-            'TSP': phos,
-            'MOP': pot
+    def calculate_fertilizer(deficit, fertilizer_type):
+        if deficit <= 10:
+            return 0
+        if fertilizer_type == 'Urea':
+            return round(min(deficit / 2.5, 78), 1)
+        elif fertilizer_type == 'TSP':
+            return min((deficit // 10 + 1) * 18, 180)
+        elif fertilizer_type == 'MOP':
+            return min((deficit // 10 + 1) * 6, 60)
+        return 0
+
+    def get_recommended_levels(crop):
+        recommendations = {
+            'maize': {'N': 197, 'P': 70, 'K': 180},
+            'rice': {'N': 175, 'P': 87, 'K': 178},
+            'kidneybeans': {'N': 138, 'P': 222, 'K': 166},
+            'mungbean': {'N': 66, 'P': 83, 'K': 100},
+            'banana': {'N': 660, 'P': 303, 'K': 1513},
+            'mango': {'N': 51, 'P': 25, 'K': 76},
+            'orange': {'N': 244, 'P': 73, 'K': 220},
+            'papaya': {'N': 27, 'P': 97, 'K': 27},
+            'coconut': {'N': 188, 'P': 125, 'K': 250},
+            'coffee': {'N': 27, 'P': 97, 'K': 27}
         }
-    elif prediction == 'rice':
-        nit = 175 - int(features[0])
-        phos = 87 - int(features[1])
-        pot =  178 - int(features[2])
-        
-        if nit > 10:
-            nit = 7.8
-        elif nit > 20:
-            nit = 15.6
-        elif nit > 30:
-            nit = 23.4
-        elif nit > 40:
-            nit = 31.2
-        elif nit > 50:
-            nit = 39
-        elif nit > 60:
-            nit = 46.8
-        elif nit > 70:
-            nit = 54.6
-        elif nit > 80:
-            nit = 62.4
-        elif nit > 90:
-            nit = 70.2
-        elif nit > 100:
-            nit = 78
-            
-        if phos > 10:
-            phos = 18
-        elif phos > 20:
-            phos = 36
-        elif phos > 30:
-            phos = 54
-        elif phos > 40:
-            phos = 72
-        elif phos > 50: 
-            phos = 90
-        elif phos > 60:
-            phos = 108
-        elif phos > 70:
-            phos = 126
-        elif phos > 80:
-            phos = 144
-        elif phos > 90:
-            phos = 162
-        elif phos > 100:
-            phos = 180
-            
-        if pot > 10:
-            pot = 6
-        elif pot > 20:
-            pot = 12
-        elif pot > 30:
-            pot = 18
-        elif pot > 40:
-            pot = 24
-        elif pot > 50: 
-            pot = 30
-        elif pot > 60:
-            pot = 36
-        elif pot > 70:
-            pot = 42
-        elif pot > 80:
-            pot = 48
-        elif pot > 90:
-            pot = 54
-        elif pot > 100:
-            pot = 60
-        response['needed_nutrients'] = {
-            'Urea': nit,
-            'TSP': phos,
-            'MOP': pot
+        return recommendations.get(crop)
+
+    recommended = get_recommended_levels(prediction)
+
+    if recommended:
+        current = {'N': int(features[0]), 'P': int(features[1]), 'K': int(features[2])}
+        deficit = {k: max(recommended[k] - current[k], 0) for k in recommended}
+
+        fertilizers = {
+            'Urea': calculate_fertilizer(deficit['N'], 'Urea'),
+            'TSP': calculate_fertilizer(deficit['P'], 'TSP'),
+            'MOP': calculate_fertilizer(deficit['K'], 'MOP')
         }
+        response['needed_nutrients'] = fertilizers
+
     return jsonify(response)
 
 if __name__ == '__main__':
